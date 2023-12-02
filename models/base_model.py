1429983ca6262827a802bf4561da92c8440f90fa
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-02 12:19:38 +0100

    fix basemodel init

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
