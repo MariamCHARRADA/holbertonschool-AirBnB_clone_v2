5e96680228af74a16758f810017022617d1625b3
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-04 00:49:23 +0100

    fix class init

--- models/base_model.py
+++ models/base_model.py
@@ -17,15 +17,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -47,8 +48,8 @@ class BaseModel:
         dictionary.update({"__class__": (str(type(self)).split(".")[-1]).split("'")[0]})
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
-        if dictionary['_sa_instance_state']:
-            del dictionary['_sa_instance_state']
+        if dictionary["_sa_instance_state"]:
+            del dictionary["_sa_instance_state"]
         return dictionary
 
     def delete(self):
