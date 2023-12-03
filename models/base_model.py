7b8b354eafadd3d4d30dd6839a55b9485d612058
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-04 00:46:45 +0100

    update to_dict()

--- models/base_model.py
+++ models/base_model.py
@@ -47,6 +47,8 @@ class BaseModel:
         dictionary.update({"__class__": (str(type(self)).split(".")[-1]).split("'")[0]})
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
+        if dictionary['_sa_instance_state']:
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
