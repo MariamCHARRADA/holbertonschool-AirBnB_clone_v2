42448a5b0927c562625d46c4824dbf2e2738a099
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-14 18:03:40 +0100

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns HBNB!"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """returns C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is_cool"):
+    """returns Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
