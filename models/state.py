fed3d02c4a7ca4ca8bb6cb1fa32b0262201a0b9f
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-15 13:16:40 +0100

    add list(storage.all)

--- models/state.py
+++ models/state.py
@@ -24,7 +24,7 @@ class State(BaseModel, Base):
             from models.city import City
 
             city_list = []
-            for city in storage.all(City).values():
+            for city in list(storage.all(City).values()):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
