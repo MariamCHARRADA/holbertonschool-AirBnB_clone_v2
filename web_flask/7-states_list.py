3bf9d68f23b96a1dee6576fc0c9c2d64c3a529bf
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-22 02:50:42 +0100

    888

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
 
 
 app = Flask(__name__)
@@ -20,6 +19,7 @@ def states_list():
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage on teardown"""
+    from models import storage
 
     storage.close()
 
