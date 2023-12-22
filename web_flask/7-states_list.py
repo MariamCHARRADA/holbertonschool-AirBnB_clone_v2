0abdc7eed57b0855527a5156c2eeb6f3299f7094
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-22 01:46:55 +0100

    ???

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
 
 
 app = Flask(__name__)
@@ -10,6 +9,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """returns a list of all states in the database"""
+    from models import storage
 
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
