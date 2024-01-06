f10dd232773105942d8319d80abc52a17b74318b
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-06 21:30:02 +0100

    task 10

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application and
-display a HTML page like 6-index.html,which was done
-during the project AirBnB clone - Web static"""
+"""Script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
-from models.state import State
+
+
 app = Flask(__name__)
 
 
 @app.route("/hbnb_filters", strict_slashes=False)
-def show():
-    """ display a HTML page like 6-index.html,
-    which was done during the project
-    AirBnB clone - Web static"""
+def hbnb_filters():
+    """Displays the main HBnB filters HTML page"""
+    from models import storage
+    from models.state import State
     from models.amenity import Amenity
-    states = list(storage.all(State).values())
-    amenities = list(storage.all(Amenity).values())
-    return render_template(
-        "10-hbnb_filters.html",
-        states=states,
-        amenities=amenities)
+
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
 def teardown(exception):
-    """remove the current SQLAlchemy Session"""
+    """Remove the current SQLAlchemy session"""
+    from models import storage
+
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
